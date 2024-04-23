7e1c0639a6cc59c3f2a7f9484c485f46a1f70195
daivid-1 <daivid.aba@gmail.com>
2024-04-24 00:53:46 +0300

    Abbreviation of the check function, division into sub-functions

--- 4 in row.py
+++ 4 in row.py
@@ -1,86 +1,81 @@
-from colorama import init, Fore, Style
-init()
+from colorama import init, Fore, Style, Back
+init(autoreset=True)
 
 # פונקציית בדיקה אם יש זכיה
-def Check(C,R,P):
+def Check(current_column,current_row,current_player):
     # מוודא שורה אופקית מלאה
-    for i in range(7):
-        if(P == cubic[R][i] == cubic[R][i+1] == cubic[R][i+2] == cubic[R][i+3]):
+    for column in range(current_column-3,current_column+1):
+        if(-1 < column < 7 and current_player == cubic[current_row][column] == cubic[current_row][column+1] == cubic[current_row][column+2] == cubic[current_row][column+3]):
             return True
     # מוודא שורה אנכית מלאה
-    if((R<7) and P == cubic[R][C] == cubic[R+1][C] == cubic[R+2][C] == cubic[R+3][C]):
+    if current_row<7 and (current_player == cubic[current_row][current_column] == cubic[current_row+1][current_column] == cubic[current_row+2][current_column] == cubic[current_row+3][current_column]):
         return True
     # מוודא שורה אלכסונית יורדת לימין מלאה
-    c = r = 0
-    if(C>=R):
-        c = C-R
-    else:
-        r = R-C
-    while(c+3 < 10 and r+3 < 10):
-        if(P == cubic[r][c] == cubic[r+1][c+1] == cubic[r+2][c+2] == cubic[r+3][c+3]):
+    for row,column in zip(range(current_row-3,current_row+1), range(current_column-3,current_column+1)):
+        if 7 > column > -1 and 7 > row > -1 and (current_player == cubic[row][column] == cubic[row+1][column+1] == cubic[row+2][column+2] == cubic[row+3][column+3]):
             return True
-        c += 1
-        r += 1
-    # מוודא שורה אלכסונית יורדת לשמאל מלאה 
-    r = 0
-    c = 9
-    if(C+R<10):
-        c = C+R
-    else:
-        r = R+C-9
-    while(c-3 > -1 and r+3 < 10):
-        if(P == cubic[r][c] == cubic[r+1][c-1] == cubic[r+2][c-2] == cubic[r+3][c-3]):
+    # מוודא שורה אלכסונית יורדת לשמאל מלאה
+    for row,column in zip(range(current_row-3,current_row+1), range(current_column+3,current_column-1,-1)):
+        if 10 > column > -1 and 7 > row > -1 and (current_player == cubic[row][column] == cubic[row+1][column-1] == cubic[row+2][column-2] == cubic[row+3][column-3]):
             return True
-        c -= 1
-        r += 1
     return False
 
 # פונקציית הדפסת מבנה המשחק  
 def Display():
-    pice = [Fore.RED + ' O ',Fore.GREEN + ' O ',Fore.YELLOW + ' - ']
+    pice = [Fore.RED + '● ',Fore.GREEN + '● ',Fore.YELLOW + '- ']
     # מבנה המשחק
-    print(F'\n       ',Fore.CYAN +'_'*39)
-    for i in range(10):
+    print('\n       ',Fore.CYAN + '_'*49)
+    for row in range(10):
         print('       ', end = '')
-        for j in range(10):
-                print(Fore.CYAN + '|'+pice[cubic[i][j]] ,end = '')
-        print(Fore.CYAN +'|')
+        for column in range(10):
+                print(Fore.CYAN + '│',pice[cubic[row][column]] ,end = '')
+        print(Fore.CYAN +'│')
     # תחתית
-    print('       |',Fore.YELLOW +'1   2   3   4   5   6   7   8   9  10', Fore.CYAN + '|\n      /','='*39,'\\\n     ','|'*43)
+    print(Fore.CYAN +'       │',end = '')
+    for i in range(1,10):
+        print(' '+Fore.YELLOW + str(i) , end = '  ')
+    print(Fore.YELLOW +'10',Fore.CYAN +'│')
+
+# מוודא קבלת ערך עמודה תקף מהמשתמש
+def get_valid_column(players_name,player):
+    while True:
+        try:
+            column = int(input(F'\n{players_name[player]} Enter the column you chose (1 to 10): ')) - 1
+            if column < 0 or column > 9:
+                print("Invalid column. Please enter a number between 1 and 10.")
+            elif cubic[0][column] != -1:
+                print('column full. Please chose anather column.')
+            else:
+                return column
+        except ValueError:
+            print("Invalid input. Please enter a number.")
+
+# פונקציית הצבה במיקום הנבחר
+def Placement(useIN,player):
+    for row in range(9,-1,-1):
+        if (cubic[row][useIN] == -1):
+            cubic[row][useIN] = player
+            Display()
+            return row
 
 # פונקציית המשתמש
 def User():
     players_name = [Fore.RED + input('\nFirst player enter your name:  ' + Fore.RED) + Style.RESET_ALL  ,  Fore.GREEN + input(Style.RESET_ALL + 'second player enter your name:  '+ Fore.GREEN) + Style.RESET_ALL]
     Display()
-    player = turn = 0
+    player = 0
     # לולאת ריצה עד סיום המערך 10*10 או בניצחון
-    while(turn < 100):
-        # קבלת מיקום עמודה מהמשתמש
-        try:
-            useIN = int(input(F'\n{players_name[player]} enter the column you chose:  '))-1
-            if useIN == -1: useIN = 9
-        except ValueError:
-            print('wirde...')
-            continue
-        # בדיקה שהערכים שהוזנו בטווח האפשרי - והאם העמודה מלאה
-        if (useIN > 9 or useIN < 0 or cubic[0][useIN] != -1):
-            print(Fore.RED + '   >> Error !!!'+ Fore.BLUE + '\n   - Either the column is already full\n   - or the number entered is not between 1-10\n\ntry again >>')
-            continue
-        # לולאת הצבה במערך במיקום הפנוי בעמודת בחירת המשתמש
-        for i in range(9,-1,-1):
-            if(cubic[i][useIN] == -1):
-                cubic[i][useIN] = player
-                row = i
-                Display()
-                # בדיקת אפשרות זכיה מתור 7 ומעלה
-                if(turn > 5 and Check(useIN,row,player)):
-                    print(F'\n\n{players_name[player]} congratulation, you are the winer!\n')
-                    return
-                break
+    for turn in range(100):
+        # קבלת מיקום עמודה תקף מהמשתמש
+        useIN = get_valid_column(players_name,player)
+        # הצבה במקום הנבחר
+        row = Placement(useIN,player)
+        # בדיקת זכיה
+        if(turn > 5 and Check(useIN,row,player)):
+            print(F'\n\n{players_name[player]} congratulation, you are the winer!\n')
+            return
         player = (player + 1) %2
-        turn += 1
     # בסיום הריצה ללא מנצחים
-    print(Style.RESET_ALL + '\n   >> ther are no winers!\n\n  ',Fore.GREEN + '>> try again...\n\n')
+    print('\n   >> ther are no winers!\n\n  ',Fore.GREEN + '>> try again...\n\n')
 
 # # # # Main # # # #       
 cubic = [[-1 for j in range(10)] for i in range(10)]
